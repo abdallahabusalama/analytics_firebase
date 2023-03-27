0a0e2fe8c3e3b04325d0e0b320e22fd8bbd6b36a
abdallahabusalama <abdallahabusalama888@gmail.com>
2023-03-27 21:50:24 +0300

    update assignment

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,41 +1,48 @@
 package com.example.myapplication
 
-import android.app.Activity
-import android.app.ProgressDialog
-import android.content.Intent
-import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.MediaStore
-import android.util.Log
-import android.widget.Toast
 import com.example.myapplication.databinding.ActivityMainBinding
-import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.analytics.ktx.analytics
+import com.google.firebase.analytics.ktx.logEvent
 import com.google.firebase.ktx.Firebase
-import com.google.firebase.messaging.FirebaseMessaging
-import com.google.firebase.storage.ktx.storage
 import java.util.*
-import kotlin.math.log
 
 
 @Suppress("DEPRECATION")
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var analytics: FirebaseAnalytics
     lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        analytics = Firebase.analytics
 
         binding.button.setOnClickListener{
-            
+
+            selectContentEvent("image1 ", "ImageView", "image")
         }
 
         binding.button3.setOnClickListener{
+            TrackScreenEventP()
+        }
 
         }
 
+    fun selectContentEvent(id:String, name:String, contentType:String) {
+        analytics.logEvent(FirebaseAnalytics.Event.SELECT_CONTENT) {
+            param(FirebaseAnalytics.Param.ITEM_ID, id);
+            param(FirebaseAnalytics.Param.ITEM_NAME, name);
+            param(FirebaseAnalytics.Param.CONTENT_TYPE, contentType);
+        }
+    }
+    fun TrackScreenEventP(){
+        analytics.logEvent(FirebaseAnalytics.Event.SCREEN_VIEW) {
+            param(FirebaseAnalytics.Param.SCREEN_NAME, "main");
+            param(FirebaseAnalytics.Param.SCREEN_CLASS, "MainActivity");
+        }
+    }
 
-           }
 }
