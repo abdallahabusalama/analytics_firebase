d7147568bd60bb5655bcf0c208516337f662dc76
abdallahabusalama <abdallahabusalama888@gmail.com>
2023-03-27 21:03:56 +0300

    init push

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.myapplication
+
+import android.app.Activity
+import android.app.ProgressDialog
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
+import android.widget.Toast
+import com.example.myapplication.databinding.ActivityMainBinding
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.messaging.FirebaseMessaging
+import com.google.firebase.storage.ktx.storage
+import java.util.*
+import kotlin.math.log
+
+
+@Suppress("DEPRECATION")
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.button.setOnClickListener{
+            
+        }
+
+        binding.button3.setOnClickListener{
+
+        }
+
+
+           }
+}
